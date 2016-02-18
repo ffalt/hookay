704a1be86ebbcd10a25d124525c95cac274793a3
ffalt <ffalty@gmail.com>
2016-02-18 12:31:02 +0100

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # hookay
 listen to webhooks, pull git, call jekyll (and a little web app to view the state)
+
+
+##Requirements
+
+[http://nodejs.org/](http://nodejs.org/)
+
+[http://bower.io/](http://bower.io/)
+
+
+
+##Installation
+
+run `npm install`
+
+copy `config.json.dist` to 'config.json' and fill in your settings
+
+run `bower install` in `/web/assets/`
+
+
+
+## Run
+
+run `node hookay.js`
+
+
---
+++ hookay.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+"use strict";
+
+var path = require('path');
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var crypto = require('crypto');
+var socketio = require('socket.io');
+var Executer = require('./lib/executer');
+
+var config = require('./config.json');
+
+var app = express();
+var server = http.Server(app);
+var io = socketio.listen(server);
+
+var executer = new Executer(config, function (o) {
+	io.emit('notify', {state: o.state, name: o.task.name});
+});
+
+var verify = function (req, res, buffer) {
+	if (!config.secret || !req.headers['x-hub-signature']) return;
+	var hmac = crypto.createHmac('sha1', config.secret);
+	var recieved_sig = req.headers['x-hub-signature'].split('=')[1];
+	var computed_sig = hmac.update(buffer).digest('hex');
+	if (recieved_sig !== computed_sig) {
+		var err = new Error('Invalid Signature :.(');
+		err.status = 403;
+		throw err;
+	}
+};
+
+app.use(bodyParser.urlencoded({extended: true, verify: verify}));
+app.use(bodyParser.json({verify: verify}));
+
+app.use(express.static('web'));
+
+app.post('/hooks/*', function (req, res) {
+	executer.exec({
+		data: req.body,
+		name: req.params
+	});
+});
+
+io.on('connection', function (socket) {
+	socket.emit('hello', {hello: 'you'});
+
+	var sendList = function () {
+		var states = executer.states();
+		var sites = config.sites.map(function (site) {
+			return {name: site.name, state: states[site.name] || 'invalid'};
+		});
+		socket.emit('list', {list: sites});
+	};
+
+	socket.on('list', function (data) {
+		sendList();
+	});
+
+	socket.on('details', function (data) {
+		socket.emit('details', executer.details(data.name));
+	});
+
+	socket.on('rebuild', function (data) {
+		var site = config.sites.filter(function (s) {
+			return s.name === data.name;
+		})[0];
+		if (site) {
+			executer.exec({
+				data: {},
+				name: site.name
+			});
+			sendList();
+		}
+	});
+});
+
+server.listen(config.server.port, config.server.host, function () {
+	console.log('Listening on port http://' + config.server.host + ':' + config.server.port);
+});
+
